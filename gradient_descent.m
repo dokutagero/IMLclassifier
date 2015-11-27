d7f9049f87e767ed989ce505b1baaa2a188f4613
dokutagero <juanjo.rubio@est.fib.upc.edu>
2015-11-28 00:13:29 +0100

    Forget about the classifier preprocessor, it works for a small learning rate of 0.00001

--- gradient_descent.m
+++ gradient_descent.m
@@ -14,14 +14,14 @@ costFunction = zeros(maxIter,1);
 
 while iter<=maxIter
     % errors = f(x;w)-y
-    errors = X'*w-y;
+    errors = y-X'*w;
     % CostFunction = 1/2N*sum(f(x;w)-y)^2). We are trying to minimize this
     % function that represents the error of the predicted vs the labeled.
     costFunction(iter) = getCostFunction(X,w,y);
     % grad is the gradient of the cost function. The minus sign represents
     % the descending direction of the gradient.
     %grad = (1/N)*errors*X;
-    grad = (1/N)*X*errors;
+    grad = -(1/N)*X*errors;
     % t describes the step size when descending to the minimum following
     % the gradient.
     w = w - t*grad;
